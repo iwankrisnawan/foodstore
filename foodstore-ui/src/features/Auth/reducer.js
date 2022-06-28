cb60c880d5732cd55cea21e8dc1f5ae8d1b515d8
iwankrisnawan <iwankrisnawan03@gmail.com>
2022-06-28 23:45:57 +0800

    test

--- foodstore-ui/src/features/Auth/reducer.js
+++ foodstore-ui/src/features/Auth/reducer.js
@@ -1,19 +1,18 @@
-import {USER_LOGIN, USER_LOGOUT} from './constants';
-let initialState = localStorage.getItem('auth')
-	?JSON.parse(localStorage.getItem('auth'))
-	:{user:null, token:null};
-	// jika local storage ada maka, jika tidak ada maka :{user:null, token:null};
+import { USER_LOGIN, USER_LOGOUT } from "./constants";
+let initialState = localStorage.getItem("auth")
+  ? JSON.parse(localStorage.getItem("auth"))
+  : { user: null, token: null };
+// jika local storage ada maka, jika tidak ada maka :{user:null, token:null};
 
-export default function reducer(state = initialState, action){
-	switch (action.type) {
+export default function reducer(state = initialState, action) {
+  switch (action.type) {
+    case USER_LOGIN:
+      return { user: action.user, token: action.token };
 
-		case USER_LOGIN:
-			return {user:action.user, token:action.token};
+    case USER_LOGOUT:
+      return { user: null, token: null };
 
-		case USER_LOGOUT:
-			return {user:null, token:null};
-
-		default:
-			return state;
-	}
+    default:
+      return state;
+  }
 }
